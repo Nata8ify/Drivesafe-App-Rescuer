7dfbfe279e76becbcd072d3841be3b033de13f33
NATTAWUT PICHAIWATTNPHON <nata8ify@gmail.com>
2017-08-02 22:53:29 +0700

    ADD Map

--- app/src/main/java/com/sitsenior/g40/weewhorescuer/fragments/NavigatorFragment.java
+++ app/src/main/java/com/sitsenior/g40/weewhorescuer/fragments/NavigatorFragment.java
@@ -7,21 +7,42 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.sitsenior.g40.weewhorescuer.R;
 
 /**
  * Created by PNattawut on 01-Aug-17.
  */
 
-public class NavigatorFragment extends Fragment {
+public class NavigatorFragment extends Fragment implements OnMapReadyCallback {
 
-    private static final String N8IFY_GOOGLE_MAPS_API_KEY = "AIzaSyAq29rswALv-iwMrEDAsrxAQcaEDWLCKcI";
-
+    private static final String N8IFY_GOOGLE_MAPS_API_KEY = "AIzaSyBz4yyNYqj3KNAl_cn2DpbIEne_45J9KTQ";
+    private GoogleMap mMap;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_navigator, container, false);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+//        SupportMapFragment supportMapFragment = (SupportMapFragment)getFragmentManager().findFragmentById(R.id.map);
+//        supportMapFragment.getMapAsync(this);
+    }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
