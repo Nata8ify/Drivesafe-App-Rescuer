25981fc0030b1161ee188ea2aa6c709cc8f874de
NATTAWUT PICHAIWATTNPHON <nata8ify@gmail.com>
2017-10-15 19:07:04 +0700

    MAKE Alternative Logout

--- app/src/main/java/com/sitsenior/g40/weewhorescuer/MainActivity.java
+++ app/src/main/java/com/sitsenior/g40/weewhorescuer/MainActivity.java
@@ -12,6 +12,9 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.sitsenior.g40.weewhorescuer.cores.AccidentFactory;
@@ -20,9 +23,12 @@ import com.sitsenior.g40.weewhorescuer.cores.LocationFactory;
 import com.sitsenior.g40.weewhorescuer.cores.WaitLocationAsyncTask;
 import com.sitsenior.g40.weewhorescuer.cores.Weeworh;
 import com.sitsenior.g40.weewhorescuer.fragments.OverviewFragment;
+import com.sitsenior.g40.weewhorescuer.models.Profile;
 import com.sitsenior.g40.weewhorescuer.services.GoingService;
 import com.sitsenior.g40.weewhorescuer.utils.DialogUtils;
 import com.sitsenior.g40.weewhorescuer.utils.SettingUtils;
+
+import io.realm.Realm;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,10 +43,15 @@ public class MainActivity extends AppCompatActivity {
 
     private Handler mainHandler;
 
+    Realm realm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Realm.init(this);
+        realm = Realm.getDefaultInstance();
 
         LocationFactory.getInstance(this);
         AddressFactory.getInstance(this);
@@ -86,8 +97,37 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
     /* Override Other Listener */
     boolean confirmOne = false;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_logout :
+                if(AccidentFactory.getResponsibleAccident() != null){
+                    Toast.makeText(this, getString(R.string.warn_responsible_logout), Toast.LENGTH_LONG).show();
+                    return true;
+                }
+                realm.executeTransaction(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        realm.delete(Profile.class);
+                        finish();
+                        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    }
+                });
+                return true;
+            case R.id.menu_screen_on : return true;
+            default : return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     public void onBackPressed() {
